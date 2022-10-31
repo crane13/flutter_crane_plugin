5db0a8e00443b9f125a8e2bfdb9a98d2de515832
crane yu <xyyh0116@aliyun.com>
2022-10-31 08:19:12 +0800

    update

--- code/crane_plugin/lib/framwork/utils/track/UmengUtils.dart
+++ code/crane_plugin/lib/framwork/utils/track/UmengUtils.dart
@@ -1,5 +1,5 @@
 import 'package:crane_plugin/framwork/utils/PlatformUtils.dart';
-import 'package:fl_umeng/fl_umeng.dart';
+// import 'package:fl_umeng/fl_umeng.dart';
 
 class UmengUtils {
   static var UMENG_KEY_ANDROID = '';
@@ -13,32 +13,32 @@ class UmengUtils {
   static Future<void> initUmeng() async {
     Future.delayed(Duration(seconds: 1), () async {
       if (PlatformUtils.isAndroid() || PlatformUtils.isIOS()) {
-        final bool data = await FlUMeng().init(
-            preInit: true,
-            androidAppKey: UMENG_KEY_ANDROID,
-            iosAppKey: UMENG_KEY_IOS,
-            channel: 'channel');
-        print('Umeng 初始化成功 = $data');
-        await FlUMeng().setLogEnabled(true);
+        // final bool data = await FlUMeng().init(
+        //     preInit: true,
+        //     androidAppKey: UMENG_KEY_ANDROID,
+        //     iosAppKey: UMENG_KEY_IOS,
+        //     channel: 'channel');
+        // print('Umeng 初始化成功 = $data');
+        // await FlUMeng().setLogEnabled(true);
       }
     });
   }
 
   static void trackEventUmeng(String event) async {
     if (PlatformUtils.isAndroid() || PlatformUtils.isIOS()) {
-      final bool data = await FlUMeng().onEvent(event, <String, String>{});
+      // final bool data = await FlUMeng().onEvent(event, <String, String>{});
     }
   }
 
   static void onPause(String page) async {
     if (PlatformUtils.isAndroid() || PlatformUtils.isIOS()) {
-      await FlUMeng().onPageEnd(page);
+      // await FlUMeng().onPageEnd(page);
     }
   }
 
   static void onResume(String page) async {
     if (PlatformUtils.isAndroid() || PlatformUtils.isIOS()) {
-      await FlUMeng().onPageStart(page);
+      // await FlUMeng().onPageStart(page);
     }
   }
 }
